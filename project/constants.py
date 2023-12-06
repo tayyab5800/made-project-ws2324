1bb21a370d874b13f6cfa2721c1fd60967a46f55
tayyab <m.tayyab@iib-institut.de>
2023-12-06 14:17:18 +0100

    moved urls into constants.py file

---
+++ project/constants.py
@@ -0,0 +1,14 @@
+urls = [
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/1-cpi-germany-individual-consumption-by-purpose-61111-0004.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/2-1-cpi-12-factors-monthly-levels.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/2-2-cpi-12-factors-change-on-previous-months.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/2-3-cpi-12-factors-annual-avg.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/2-4-cpi-12-factors-change_on_prev_years.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/3-interest-rate.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/4-1-cpi-sp-energy-data.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/4-2-cpi-sp-energy-data-annual-avg.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/4-3-cpi-sp-energy-dist-data.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/5-housing-prices-6-quarters-61262-0002.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/6-1-property-price-indices-district-types.csv",
+        "https://raw.githubusercontent.com/tayyab5800/german-housing-prices-drop/main/6-2-property-price-indices-rate-of-change.csv"
+        ]
